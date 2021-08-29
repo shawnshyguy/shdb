93bbde902cdb31c513656e703fa5f53e6c488798
Shawn <marioman1203024@gmail.com>
2021-08-29 01:19:29 -0600

    maybe a real fix???

---
+++ for contributors/example.js
@@ -0,0 +1,13 @@
+export default
+{
+    "Friendly": "Nest Mini (2nd gen)",
+    "Model": "Google Nest Mini (2nd Generation)",
+    "Manufacturer": "Google",
+    "Type": "Speaker",
+    "Tech": [
+        "WiFi"
+    ],
+    "Software": [
+        "Google Home"
+    ]
+}
